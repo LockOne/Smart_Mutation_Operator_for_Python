a68852437700872b14d8eaefceca8d1c873861d2
LockOne <dkcjd2000@gmail.com>
2018-06-12 17:25:48 +0000

    fix checkErr

--- src/checkErr.py
+++ src/checkErr.py
@@ -4,12 +4,15 @@ inputfile = open(sys.argv[1], 'r')
 errtypes = ["SyntaxError", "IndentationError", "ImportError", "NameError", "ValueError"]
 errs = {}
 for line in inputfile:
-		for errT in errtypes:
-				if errT in errs.keys():
-						errs[errT] += 1
-						break
-				else:
-						errs[errT] = 1
+    for errT in errtypes:
+        if errT in line:
+            if errT in errs.keys():
+                errs[errT] += 1
+	    else:
+	        errs[errT] = 1
+            break
+
+
 for er in errs.keys():
     print(er +  " : " + str(errs[er]))
 inputfile.close()
